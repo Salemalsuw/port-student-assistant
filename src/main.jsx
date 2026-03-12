7007f61f5fab8a3c4097a3261b2495449c28cb96
Salemalsuw <smsalsuwaida@gmail.com>
2026-03-12 03:57:01 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
-export default function PortStudentAssistantApp() {
-  return (
-    <div dir="rtl" style={{fontFamily:"sans-serif",padding:"40px",textAlign:"center"}}>
-      <h1>منصة مسابقة الموانئ</h1>
-      <p>مرحبًا بك في منصة إدارة المسابقات</p>
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-      <div style={{marginTop:"30px"}}>
-        <button style={{padding:"10px 20px",margin:"10px"}}>التسجيل</button>
-        <button style={{padding:"10px 20px",margin:"10px"}}>عرض المسابقات</button>
-        <button style={{padding:"10px 20px",margin:"10px"}}>النتائج</button>
-      </div>
-    </div>
-  )
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
