0d84a7b9646bd2e112cee397ad314a3efe06aadf
Salemalsuw <smsalsuwaida@gmail.com>
2026-03-12 03:02:56 +0300

    Create PortStudentAssistantApp.jsx

---
+++ src/PortStudentAssistantApp.jsx
@@ -0,0 +1,8 @@
+export default function PortStudentAssistantApp() {
+  return (
+    <div style={{padding:40,fontFamily:"Tahoma",textAlign:"center"}}>
+      <h1>منصة مسابقة الموانئ</h1>
+      <p>تم تشغيل React بنجاح ✅</p>
+    </div>
+  );
+}
